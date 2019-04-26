3c8ca9842e285b554ff52ea9212fd286ac8cac1d
Andy <andrewprohde@gmail.com>
2019-04-26 13:52:17 -0500

    Added bounds

--- src/com/tutorial/main/HUD.java
+++ src/com/tutorial/main/HUD.java
@@ -7,6 +7,9 @@ public class HUD {
     public static int HEALTH = 100;
     public void tick() {
 
+        // Health Test
+        //HEALTH--;
+        HEALTH = Game.clamp(HEALTH, 0, 100);
     }
 
     public void render(Graphics g) {
@@ -14,6 +17,8 @@ public class HUD {
         g.fillRect(15, 15, 200, 32);
         g.setColor(Color.green);
         g.fillRect(15, 15, HEALTH * 2, 32);
+        g.setColor(Color.white);
+        g.drawRect(15, 15, 200, 32);
 
     }
 }
